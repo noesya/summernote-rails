3581d868bbe303377f37f6e26858176debae51a4
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-02-11 12:46:40 +0100

    data-attributes for direct upload

--- lib/summernote-rails/simple_form/summernote_input.rb
+++ lib/summernote-rails/simple_form/summernote_input.rb
@@ -1,7 +1,11 @@
 class SummernoteInput < SimpleForm::Inputs::TextInput
   def input(wrapper_options)
     input_html_options[:data] ||= {}
-    input_html_options[:data].merge!({ provider: 'summernote' })
+    input_html_options[:data].merge!({
+      provider: 'summernote',
+      direct_upload_url: @builder.template.rails_direct_uploads_url,
+      blob_url_template: @builder.template.rails_service_blob_url(":signed_id", ":filename")
+    })
     super
   end
 end
