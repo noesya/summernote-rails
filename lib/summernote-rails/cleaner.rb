3ec3acf128af2276891efeb806159c90bb60ec15
Arnaud Levy <contact@arnaudlevy.com>
2022-05-19 19:40:43 +0200

    do not clean, it's not correct enough

--- lib/summernote-rails/cleaner.rb
+++ lib/summernote-rails/cleaner.rb
@@ -1,5 +1,8 @@
 class SummernoteCleaner
+  BLOCK_TAGS = 'h1, h2, h3, h4, h5, h6, p, ul, ol, dl'
+
   def self.clean(code)
+    return code
     clean_code = code
     unless clean_code.start_with? '<p>'
       chunks = clean_code.split '<p>'
--- lib/summernote-rails/version.rb
+++ lib/summernote-rails/version.rb
@@ -1,5 +1,5 @@
 module SummernoteRails
   module Rails
-    VERSION = "0.8.21"
+    VERSION = "0.8.22"
   end
 end
